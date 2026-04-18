4c316e9b385e729270f4313cad6ed8198fdcf1af
iurii-kyrylenko <yuriproject@gmail.com>
2026-04-18 15:42:59 +0300

    Update to latest versions. Add RSC plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { tanstackStart } from "@tanstack/react-start/plugin/vite"
-import { defineConfig } from "vite"
-import tsConfigPaths from "vite-tsconfig-paths"
-import viteReact from "@vitejs/plugin-react"
-import tailwindcss from "@tailwindcss/vite"
-import { nitro } from "nitro/vite"
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
+import rsc from "@vitejs/plugin-rsc";
+
 export default defineConfig({
     server: {
         port: 3000,
@@ -15,7 +17,11 @@ export default defineConfig({
         }),
         tanstackStart({
             srcDirectory: "src",
+            rsc: {
+                enabled: true,
+            },
         }),
+        rsc(),
         viteReact(),
         nitro(),
     ],
